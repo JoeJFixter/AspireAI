f0c4ada7944c00dbf74083558a96730a69c34519
Zi Ang Yang <zi.yang-3@student.manchester.ac.uk>
2024-11-09 15:59:18 +0000

    set up User, Chat, Task, Story entities with basic attributes

--- back-end/app/models.py
+++ back-end/app/models.py
@@ -31,4 +31,12 @@ class Task(db.Model):
     def __repr__(self):
         return f"Task('{self.title}', '{self.description}', '{self.end_date}', '{self.completed}')"
     
-    
+
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"Story('{self.title}', '{self.description}')"
